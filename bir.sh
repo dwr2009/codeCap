bccae1b90b0597ede500086370c7787d55a448f4
luoqiao <2335975722@qq.com>
2016-10-20 15:02:54 +0800

    add a script that show the days between now and what you input

---
+++ bir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "Input your birthday(YYMMDD):" date1
+m=`date --date=$date1 +%m`
+d=`date --date=$date1 +%d`
+date_now=`date +%s`
+y=`date +%Y`
+birth=`date --date=$y$m$d +%s`
+echo "birth = $birth"
+echo "date_now = $date_now"
+internal=$(($birth-$date_now))
+echo "internal = $internal"
+if [ "$internal" -lt "0" ];then
+	birth=`date --date=$(($y+1))$m$d +%s`
+	internal=$(($birth-$date_now))
+fi
+echo "There is :$((internal/60/24)) days ."
